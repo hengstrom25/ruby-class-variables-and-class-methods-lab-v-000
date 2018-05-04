60482f3ab0c8bc9a8c3a05b2a93bad74fefef605
Heidi Engstrom <hengstrom25@gmail.com>
2018-05-04 18:32:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,9 @@ class Song
   end
   
   def self.genre_count
-    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+    @@genres.inject(Hash.new(0)) { |total, genre| total[genre] += 1 ;total}
   end
+  
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) { |total, i|
 end
